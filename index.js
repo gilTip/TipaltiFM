b41f57502fae9248c68a3dde393ca472b82b022a
Gil <gil.ariel@tipalti.com>
2023-02-13 11:46:43 +0200

    updated token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { App } = require('@slack/bolt');
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: 'xoxb-3801044593-4789184504293-R6Np4707K4JuWvwRUn5dLQvj',
+  token: 'xoxb-3801044593-4789184504293-ZbyI2moUmNeVvqb3ctikpF6m',
   signingSecret: '752f20fef8ea68db033a899448f367f4'
 });
 
